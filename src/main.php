a6db9c26c96ac42104b1989bef1c05a5a5c70c6f
tkuramot <mabdman512@gmail.com>
2024-04-03 02:32:57 +0900

    Move main.php

--- src/main.php
+++ src/main.php
@@ -1,7 +1,7 @@
 <?php
 spl_autoload(".php");
 spl_autoload_register(function ($class) {
-    $base_dir = __DIR__ . "/src/";
+    $base_dir = __DIR__ . "/";
     $file = $base_dir . str_replace("\\", "/", $class) . ".php";
     if (file_exists($file)) {
         require $file;
